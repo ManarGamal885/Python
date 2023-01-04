b859cf31928dd0ce71919f967292713942bd9ccd
Manar Gamal <manarg281@gmail.com>
2023-01-04 23:55:46 +0200

    project outlines

---
+++ dataAnalysis/week6/week6.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+file_name='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/FinalModule_Coursera/data/kc_house_data_NaN.csv'
+df = pd.read_csv(file_name)
+print(df.head(5))
