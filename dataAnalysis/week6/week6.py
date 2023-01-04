223cc0ee39d868b84e0915d0cb8d0e7ee8f2fd46
Manar Gamal <manarg281@gmail.com>
2023-01-05 00:26:45 +0200

    edit1

--- dataAnalysis/week6/week6.py
+++ dataAnalysis/week6/week6.py
@@ -8,4 +8,6 @@ from sklearn.linear_model import LinearRegression
 
 file_name='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/FinalModule_Coursera/data/kc_house_data_NaN.csv'
 df = pd.read_csv(file_name)
-print(df.head(5))
+df.head(5)
+print(df.dtypes)
+df.describe()
